1af26106b859b70cfabcf6553b33cf87732c48c2
OrestKravtsiv <orest.kravtsiv@lnu.edu.ua>
2025-04-28 00:02:11 +0300

    Доналаштував маршрутизацію

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-// /src/App.js
-import { useState } from 'react';
-import { movies as moviesData } from './data/movies';
-import MovieList from './components/MovieList';
-import './App.css';
+// src/App.jsx
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Booking from './pages/Booking';
 
 function App() {
-  const [searchQuery, setSearchQuery] = useState('');
-
-  const filteredMovies = moviesData.filter(movie =>
-    movie.title.toLowerCase().includes(searchQuery.toLowerCase())
-  );
-
   return (
     <div className="App">
       <h1>Кінотеатр</h1>
-      <input
-        type="text"
-        placeholder="Пошук фільму..."
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        className="search-input"
-      />
-      <MovieList movies={filteredMovies} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/booking/:id" element={<Booking />} />
+      </Routes>
     </div>
   );
 }
